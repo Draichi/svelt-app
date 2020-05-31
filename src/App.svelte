db816a50b8225bd0def2912572d2bd445b39aa30
draichi <lucasdraichi@gmail.com>
2020-05-31 01:28:54 -0300

    wip

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,13 @@
 	
   import MeetupsGrid from './Meetups/MeetupsGrid.svelte';
 
-  const meetups = [
+	let title = '';
+	let subtitle = '';
+	let address = '';
+	let email = '';
+	let imageUrl = '';
+	let description = '';
+  let meetups = [
     {
       id: 'm1',
       title: 'Lucas draichi',
@@ -23,10 +29,50 @@
       contactEmail: '123@gmail.com',
     },
   ]
+
+  function addMeetup() {
+	  const newMeetup = {
+		  id: Math.random().toString(),
+		  title,
+		  subtitle,
+		  description,
+		  imageUrl,
+		  contactEmail: email,
+		  address,
+	  };
+	  meetups = [newMeetup, ...meetups]
+  }
 </script>
 
 <Header/>
 
 <main>
+<form on:submit|preventDefault={addMeetup}>
+	<div class="form-control">
+		<label for="title">Title</label>
+		<input bind:value={title} type="text" id="title"/>
+	</div>
+	<div class="form-control">
+		<label for="subtitle">Subtitle</label>
+		<input bind:value={subtitle} type="text" id="subtitle"/>
+	</div>
+	<div class="form-control">
+		<label for="address">Address</label>
+		<input bind:value={address} type="text" id="address"/>
+	</div>
+	<div class="form-control">
+		<label for="email">Email</label>
+		<input bind:value={email} type="text" id="email"/>
+	</div>
+	<div class="form-control">
+		<label for="image">Image</label>
+		<input bind:value={imageUrl} type="text" id="image"/>
+	</div>
+	<div class="form-control">
+		<label for="description">Description</label>
+		<textarea bind:value={description} id="description"/>
+	</div>
+	<button type="submit">Add</button>
+</form>
 	<MeetupsGrid {meetups}/>
 </main>
